b29a3a07199a5a18df64fdbc09865b8d91ab723c
hic <minping.yang@yahoo.com>
2017-03-13 20:22:14 +1300

    guess game

---
+++ learningProcess/guess.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+
+int main(){
+  srand(time(NULL));
+  int randomNumber = (rand()%100)+1;
+  
+  char number_typedIn;
+  printf("Pick a number between 1 and 100\n");
+ 
+  scanf("Your guess:%c",&number_typedIn);
+  if(c<48 && c>57)
+  if(number_typedIn == randomNumber){
+    printf("Congradulations!");
+  }else if (num){
+    
+  }
+  return 0;
+}
+
+int isNumber(char c){
+  
+}
+
