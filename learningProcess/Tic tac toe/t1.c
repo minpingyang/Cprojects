59dead8113cb7ca24f2b174f547612b1154dfb9c
hic <minping.yang@yahoo.com>
2017-03-29 01:15:02 +1300

    draw the board in a proper way

--- learningProcess/Tic tac toe/t1.c
+++ learningProcess/Tic tac toe/t1.c
@@ -2,31 +2,45 @@
 #include <stdlib.h>
 
 void draw(const int size, char field[]);
-int add_cross(const int size, char field[], const int position);
+//int add_cross(const int size, char field[], const int position);
+//int is_solved(const int size, char field[]);
+char board[];
 
 int main(){
     int sizeF;
+    int position;
     printf("Enter the size of field: ");
     
     //user type in a size of field
     scanf("%d",&sizeF);
-    char board [sizeF];
+    board [sizeF];
     draw(sizeF, board);
+    //add_cross(sizeF,board,position);
+    
     return 0;
 }
 void draw(const int size, char field[]){
+
    int i = 1;
+   
+    //initialise field with " "space
+   i = 0;
+   while(i < size){
+      field[i++] =' ';     
+   }
+   
+   i = 1;
+   
    // draw "+-"
   while(i <= size){
    printf("+-");
    i++;
   }
   printf("+\n");
-  i = 1;
+  i = 0;
   // draw "| "
-  while(i <= size){
-   printf("| ");
-   i++;
+  while(i < size){
+   printf("|%c",field[i++]);
   }
   printf("|\n");
   i = 1;
@@ -45,7 +59,35 @@ void draw(const int size, char field[]){
   
    
 }
-
+/*
 int add_cross(const int size, char field[], const int position){
-return 0;
+//che
+   if(){
+    return 1;
+   }else if(){
+    return 0;
+   }
+   return -1;
+
 }
+
+int is_solved(const int size, char field[]){
+   if(){
+   
+   return 1;
+   }
+   return 0;
+
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
