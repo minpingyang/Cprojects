a3484f1ebf61e8c41e2e74423cfbc97a52077e69
hic <minping.yang@yahoo.com>
2017-03-29 13:30:34 +1300

    finish the add cross method

--- learningProcess/Tic tac toe/t1.c
+++ learningProcess/Tic tac toe/t1.c
@@ -1,21 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void draw(const int size, char field[]);
-//int add_cross(const int size, char field[], const int position);
-//int is_solved(const int size, char field[]);
-char board[];
+int add_cross(const int size, char field[], const int position);
+// int is_solved(const int size, char field[]);
+char board[80];
+
+
 
 int main(){
+    
     int sizeF;
     int position;
+    int is_solved = 0; //a flag judge if game solve
+    int insertPosition = 0;
     printf("Enter the size of field: ");
     
     //user type in a size of field
     scanf("%d",&sizeF);
     board [sizeF];
-    draw(sizeF, board);
-    //add_cross(sizeF,board,position);
+    draw(sizeF, board);  //  draw initial board
+
+    // while-loop to sun this gamme Player A add cross player B add ac
+    //loop end until is_solved == 1 || cross is fulled in the board (draw) || 
+    // while (is_solved != 1 ) {
+    //   printf("Player A: " );
+    //   scanf("%d",&insertPosition);
+    //   //now the board array initialized with space
+    //   add_cross(sizeF,board,insertPosition);
+    //   is_solved =1 ;
+    // }
+    printf("Player A: " );
+    scanf("%d",&insertPosition);
+      //now the board array initialized with space
+    add_cross(sizeF,board,insertPosition);
+
     
     return 0;
 }
@@ -56,30 +76,36 @@ void draw(const int size, char field[]){
    printf(" %d",i++);
   }
   printf(" \n");
-  
-   
+  printf(" \n");
+  //copy field array to board array ???
+  strcpy(board,field);
 }
-/*
+
 int add_cross(const int size, char field[], const int position){
-//che
-   if(){
+   //check if the possition is free
+   
+   if(field[position-1] == ' ' && position <= size){
+    field[position-1] = 'X';
+    draw(size, field);
     return 1;
-   }else if(){
+   }else if(field[position-1] == 'X' && position <= size){
+    printf("X is already there\n");   
     return 0;
    }
+   printf("Wrong position\n");
    return -1;
 
 }
 
-int is_solved(const int size, char field[]){
-   if(){
+// int is_solved(const int size, char field[]){
+//    if(){
    
-   return 1;
-   }
-   return 0;
+//    return 1;
+//    }
+//    return 0;
 
-}
-*/
+// }
+
 
 
 
