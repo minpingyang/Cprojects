24bf39b401551271e5b3f0b300c9e8759606306d
hic <minping.yang@yahoo.com>
2017-04-03 12:40:44 +1200

    fix the issue that if same player repeatly input same location, the programme does not report the erro

--- learningProcess/Tic tac toe/t2.c
+++ learningProcess/Tic tac toe/t2.c
@@ -5,6 +5,7 @@
 void draw(const int size, char field[][size]);
 int add_cross(const int size, char field[][size], const int x, const int y, const char player);
 int is_solved(const int size, char field[][size]);
+void checkAdding(int add_cross,const int size,char field[][size]);
 
 int sizeF;
 
@@ -26,50 +27,55 @@ int main () {
   }
   draw(sizeF,board);
      while(isSolved == 0){     
-      if(doesAddCross == 1){
-          printf("Player A: " );
-              printf("choose row: ");
-              scanf("%d",&insertPositionRow);
-              printf("choose col: ");
-              scanf("%d",&insertPositionCol);
+
+          printf("Player A: \n" );
+              
+              printf("Input (row number + space + col number): ");
+              scanf("%d %d",&insertPositionRow,&insertPositionCol);
+              printf("row: %d  col: %d\n",insertPositionRow,insertPositionCol);
 
             //now the board array initialized with space
           doesAddCross = add_cross(sizeF, board,insertPositionRow,insertPositionCol,'A');
-          
-          if (doesAddCross == 1) {
-            draw(sizeF, board);
-          }
+          checkAdding(doesAddCross,sizeF,board);
+          // if (doesAddCross == 1) {
+          //   draw(sizeF, board);
+          // }else if (doesAddCross == 0 ){
+          //   printf("X is already there!\n"); 
+          //   doesAddCross = 1;
+          // }else if (doesAddCross == -1) {
+          //     printf("Wrong position!\n");
+          //     doesAddCross = 1;
+          // }
 
           isSolved = is_solved(sizeF,board);
           if (isSolved == 1){
             printf("Player A won\n");
             break;
           }
-          printf("Player B: " );
-           printf("choose row: ");
-              scanf("%d",&insertPositionRow);
-              printf("choose col: ");
-              scanf("%d",&insertPositionCol);
+          printf("Player B: \n" );
+           
+              printf("Input (row number + space + col number): ");
+              scanf("%d %d",&insertPositionRow,&insertPositionCol);
+              printf("row: %d  col: %d\n",insertPositionRow,insertPositionCol);
 
           doesAddCross = add_cross(sizeF, board,insertPositionRow,insertPositionCol,'B');    
           
-          if (doesAddCross == 1) {
-            draw(sizeF, board);
-          }
+          checkAdding(doesAddCross,sizeF,board);
+          // if (doesAddCross == 1) {
+          //     draw(sizeF, board);
+          // }else if (doesAddCross == 0 ){
+          //     printf("X is already there!\n"); 
+          //     doesAddCross = 1;
+          // }else if (doesAddCross == -1) {
+          //     printf("Wrong position!\n");
+          //     doesAddCross = 1;
+          // }
 
           isSolved = is_solved(sizeF,board);
           if (isSolved == 1){
             printf("Player B won\n");
             break;
           }
-
-      }else if (doesAddCross == 0 ){
-          printf("X is already there!\n"); 
-          doesAddCross = 1;
-      }else if (doesAddCross == -1) {
-             printf("Wrong position!\n");
-          doesAddCross = 1;
-      }
    }    
 
 
@@ -78,10 +84,18 @@ int main () {
 
 }
 
+void checkAdding(int add_cross,const int size,char field[][size]){
+   if (add_cross == 1) {
+            draw(size, field);
+          }else if (add_cross == 0 ){
+            printf("X is already there!\n"); 
+            add_cross = 1;
+          }else if (add_cross == -1) {
+              printf("Wrong position!\n");
+              add_cross = 1;
+          }
 
-
-
-
+}
 
 
 void draw(const int size, char field[][size]){
@@ -170,125 +184,6 @@ int is_solved(const int size, char field[][size]){
    
    return 0;  
 }
-
-
-// int main(){
-//  
-//     int sizeF;
-//     int position;
-//     int isSolved = 0; //a flag judge if game solve
-//     int insertPosition = 0;
-//     int doesAddCross = 1;
-//     printf("Enter the size of field: ");
-    
-//     //user type in a size of field
-//     scanf("%d",&sizeF);
-//     char board[sizeF];
-//     int i;
-//     int count = 0; // count how many cross was added
-//     for(i = 0; i <sizeF ; i ++){
-//       board [i] = ' ';
-//     }
-//    // board [sizeF] = {' '};
-//     draw(sizeF, board);  //  draw initial board
-
-//    while(isSolved == 0){     
-//       if(doesAddCross == 1){
-//           printf("Player A: " );
-//           scanf("%d",&insertPosition);
-//             //now the board array initialized with space
-//           doesAddCross = add_cross(sizeF,board,insertPosition);
-          
-//           if (doesAddCross == 1) {
-//             draw(sizeF, board);
-//           }
-
-//           isSolved = is_solved(sizeF,board);
-//           if (isSolved == 1){
-//             printf("Player A won\n");
-//             break;
-//           }
-//           printf("Player B: " );
-//           scanf("%d",&insertPosition);
-//           doesAddCross = add_cross(sizeF,board,insertPosition);      
-          
-//           if (doesAddCross == 1) {
-//             draw(sizeF, board);
-//           }
-
-//           isSolved = is_solved(sizeF,board);
-//           if (isSolved == 1){
-//             printf("Player B won\n");
-//             break;
-//           }
-
-//       }else if (doesAddCross == 0 ){
-//           printf("X is already there!\n"); 
-//           doesAddCross = 1;
-//       }else if (doesAddCross == -1) {
-//              printf("Wrong position!\n");
-//           doesAddCross = 1;
-//       }
-//    }    
-    
-   
-//     return 0;
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// int add_cross(const int size, char field[], const int position){
-//    //check if the possition is free
-   
-//    if(field[position-1] == ' ' && position <= size){
-//     field[position-1] = 'X';
-//     // draw(size, field);
-//     return 1;
-//    }else if(field[position-1] == 'X' && position <= size){
-      
-//     return 0;
-//    }
-//    return -1;
-
-// }
-
-// int is_solved(const int size, char field[]){
-
-//    int i;
-//    for (i = 0; i < size - 2 ; i++) {
-//       if(field[i] == 'X' && field[i+1] == 'X' && field[i+2] == 'X'){
-//         return 1;
-//       }  
-//    }
-
-//    return 0;
-
-// }
 
 
 
