a7edc3d873cc150041b861482daef7984397ef69
hic <minping.yang@yahoo.com>
2017-04-13 01:23:22 +1200

    find a way to append a string only by using pointers not array

---
+++ smallImportantTest/appendStringOnlyUsingPointers.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#define SIZE 80
+void contact1(char *,char*);
+// void concat(char *,char *);
+
+int main(){
+	char string1[SIZE];
+	char string2[SIZE];
+	printf("Enter string with space :\n");
+	gets(string1);
+	contact1(string1,string2);
+}
+
+void contact1(char *str1,char * str2){
+	
+
+	char *conc = str2;
+	//delet leading space
+	
+	while(*str1 != '\0'){
+		if(*str1 == ' '){			
+			str1++;
+		}else {			
+			*str2 = *str1;
+			str1++,str2++;
+		}
+	}
+	*str2='\0';
+	puts(conc);
+}
+
+
+
+
+
+
+
+// int main()
+// {
+//     char string1[SIZE]="\0",string2[SIZE]="\0";
+
+//     printf("Enter String 1:\n");
+//     gets(string1);
+//     printf("Enter String 2:\n");
+//     gets(string2);
+
+//     concat(string1,string2);
+
+//     return 0;
+// }
+
+// void concat(char *str1,char *str2)
+// {
+//     char *conc=str1;
+
+//     while(*str1!='\0'){
+//         str1++;
+//     }
+
+//     *str1=' '; //  \0 ---> space
+//     str1++;		// after space +1 index
+
+//     while(*str2!='\0')
+//     {
+//         *str1=*str2; // add 
+//         str1++,str2++;
+//     }
+
+//     *str1='\0';
+//     printf("Concatenated String:\n");
+//     puts(conc);
+
+// }
