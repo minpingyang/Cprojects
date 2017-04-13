58982d5458a786759a03dec0f7090fb0851dc240
hic <minping.yang@yahoo.com>
2017-04-13 13:38:09 +1200

    fix the issue which is about only remove one space, modify the code to allow it to remove all space

--- learningProcess/pointerAndString/t3.c
+++ learningProcess/pointerAndString/t3.c
@@ -8,14 +8,14 @@ int main() {
 	// TODO – do not forget to read in two strings
 	// TODO – the names of the variables are strp and strq
 	char *strp;
-	
+	char strq[SIZE];
 	
 	gets(strp);
 	printf("Original strp: [%s]\n", strp);
 	spacedel_ptr(strp);
 	printf("Spaceless strp: [%s]\n", strp);
 
-	char strq[SIZE];
+
 	gets(strq);
 	printf("Original strq: [%s]\n", strq);
 	spacedel_ary(strq);
@@ -33,16 +33,32 @@ void spacedel_ptr(char *str1) {
 	// pointers instead!
 	// Do not forget that a string is concluded by a null-terminator
 
-	while(*str1 != '\0') {
-		if(*str1 == ' ') {
-			
-			char *temp = str1;
-			while(*temp != '\0'){
-				*temp = *(temp+1);
-				temp++;
+	
+	int isSpace = 0;
+	char* pointer =str1;
+	while(1) {
+		if(*str1 != '\0'){
+
+			if(*str1 == ' ') {
+				
+				char* temp = str1;
+				while(*temp != '\0'){
+					*temp = *(temp+1);
+					if(*temp == ' '){
+						isSpace = 1;
+					}
+					temp++;
+				}
 			}
+			str1++;
+		}else if(isSpace){
+			isSpace = 0;
+			str1 = pointer;
+		}else{
+			break;
 		}
-		str1++;
+
+		
 	}
 
 }
@@ -55,21 +71,32 @@ void spacedel_ary(char *t) {
 	// In this function you have to work with the string as an array,
 	// i.e. you have to use array indices
 	// Do not forget that a string is concluded by a null-terminator 
+	int isSpace = 0;
 	int i =0;
 	int j = 0;
-	while(t[i] != '\0') {
-		if(t[i] == ' ') {
-			j = i;
-			char temp = t[j];
-			while(temp != '\0'){
-				 t[j] =t[j+1];
-				 temp = t[j];
-				j++;
+	while(1) {
+		if(t[i] != '\0'){
+			if(t[i] == ' ') {
+				j = i;
+				char temp = t[j];
+				while(temp != '\0'){
+					 t[j] =t[j+1];//index of space ---> store next element
+					 temp = t[j];//temp =next element;
+					 if(temp == ' '){
+						isSpace = 1;
+					  }
+					 j++;
+				}
 			}
+			i++; 	
+		}else if (isSpace){
+			isSpace = 0;
+			i = 0;
+			j = 0;
+		}else{
+			break;
 		}
-
 		
-			i++; 
 		
 	}
 
