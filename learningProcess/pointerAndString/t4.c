14243c2039cf6e6ba61db60a8a0aa36aa2c4fc5b
hic <minping.yang@yahoo.com>
2017-04-13 21:42:43 +1200

    task 4 done which is about get the substring by the input character

--- learningProcess/pointerAndString/t4.c
+++ learningProcess/pointerAndString/t4.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
-
-char* my_strchr(char* str, char c)
-{
-	// TODO – here you implement the function
-}
+char* my_strchr(char* str, char c);
+#define SIZE 80
 
 int main()
-{
+{	
+	char str[80];
+	char c;
 	printf("Enter the string: ");
 	// TODO – here you read in the string
 	// TODO – the variable name is str
+	gets(str);
 
 	printf("Enter the character: ");
 	// TODO – here you read in the character
 	// TODO – the variable name is c
+	scanf("%c",&c);
 
 	char* result = my_strchr(str, c); 
-
+	// printf("Substring: %s \n", result);
 	if (result == NULL)
 	{
 		printf("Couldn't find %c.\n", c);
@@ -25,4 +26,21 @@ int main()
 	{
 		printf("Substring: %s \n", result);
 	}
+}
+char* my_strchr(char* str, char c)
+{
+	// TODO – here you implement the function
+
+	//remeber the first character mermory address
+	char* temp;
+	while(*str != '\0'){
+
+		if(*str == c){
+		
+		return temp = str;
+				
+		}
+		str++;
+	}
+	return NULL;
 }
