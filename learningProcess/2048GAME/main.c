c92bb4e516089e8fecbea82192af27c5da8af4ad
hic <minping.yang@yahoo.com>
2017-05-09 21:03:40 +1200

    load and save game works well

--- learningProcess/2048GAME/main.c
+++ learningProcess/2048GAME/main.c
@@ -25,8 +25,10 @@ int main(){
     //Show intructions of the game
     //(i.e. what character user should input to slide tiles )
     //(w,a,s,d)--> up,left,down,right, respectively
-    printf("\n Game intructions:\nPlayer inputs (w,a,s,d) characters to \nslide tiles up,left,down,right, respectively\n");
+    printf("\n------>>>>Game intructions<<<<<---------\nPlayer inputs a single (w,a,s,d) character to move up,left,down,right, respectively\ninput (1 or 2 )for saving and loading game respectively\n\n");
 
+    printf("\nStart Game:\n");
+    printf("Firtly, add a random character in board\n");
     //if it is possible to move further, then generate a random number into board
     bool moveNotChanged = false;
     add_random_tile(ptr_game);
@@ -36,7 +38,7 @@ int main(){
       if (!moveNotChanged&& answer == 'N')
       {
 	      //if it is possible to move further, then generate a random number into board
-	      printf("\nPossible to move, so add an random title\n");
+	      printf("\nPossible to move by input direction, so add an random title\n");
 	      add_random_tile(ptr_game);
 	      render(current_game);
       }
@@ -45,12 +47,14 @@ int main(){
 
       beforeMove_game = current_game;
 	 
-	  printf("input (w,a,s,d) below to slide titles\n");
+	  printf("Now, continue game to input (w,a,s,d) below to slide titles\n");
 	    do {
 	      answer = getchar();
 	      
 	   } while(answer != 'w' && answer != 'W'&& answer != 'a'&& answer !='A'
-	   		&&answer != 's' && answer != 'S'&& answer != 'd'&& answer !='D');
+	   		&&answer != 's' && answer != 'S'&& answer != 'd'&& answer !='D'
+	   		&&answer != '1' && answer != '2'
+	   		);
 	    
 	    //update the board(i.e. slide title),according to input character
 	    //left -> (0,-1)  right->(0,1) up ->(-1,0)  down(1,0)
@@ -78,6 +82,17 @@ int main(){
 	   	   doesUpdate = update(ptr_game,-1,0);
 	   	   answer = 'N'; // allow to add random
 	   	}
+	   	else if(answer == '1'){
+	   	   printf("\nSave current game state...............\n");
+	   	   saveGame(current_game);
+	   	   answer = 'm'; // allow to add random
+	   	}
+	   	else if(answer == '2'){
+	   	   printf("\nLoading privous game state...............\n");
+	   	   loadGame(ptr_game);
+	   	   answer = 'm'; // allow to add random
+	   	}
+
 	   	does_Move = is_move_possible(current_game); // check if it is possible to move
 	   	doesWin  = is_game_won(current_game);
 	   	if(doesWin){
