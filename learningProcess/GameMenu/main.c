83d149595eba5bc0c5c23813beb07ceb13c8817d
hic <minping.yang@yahoo.com>
2017-05-21 23:45:31 +1200

    create a new project

---
+++ learningProcess/GameMenu/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+// #include "k.h"
+
+int main(){
+	char answer =' '; // user input to slide titles;
+	//welcome note 
+	printf("\n------>>>>Welcome to the menue of games<<<<<---------\n");
+ 	
+ 	printf("Now, continue game to input (w,a,s,d) below to slide titles\n");
+	 do {
+	      answer = getchar();	      
+	   } while(answer != 'q' && answer != 'Q'&& answer != 'a'&& answer !='A'
+	   		&&answer != 's' && answer != 'S'&& answer != 'd'&& answer !='D'
+	   		&&answer != '1' && answer != '2');
+
+	  // quite the game case.
+	 if(answer == 'q' && answer == 'Q'){
+	 	return 0;
+	 }
+
+
+    return 0;
+}
+
+
