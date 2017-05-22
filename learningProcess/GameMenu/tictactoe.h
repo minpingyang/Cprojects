40e213327c5702e5aa72a5049c37c106e0f3ce1d
Minping Yang <yangminp@fiebigs.ecs.vuw.ac.nz>
2017-05-22 23:03:31 +1200

    creat a feature for all games which make all of them can save current state

--- learningProcess/GameMenu/tictactoe.h
+++ learningProcess/GameMenu/tictactoe.h
@@ -9,3 +9,4 @@ int is_solved(const int size, char field[][size]);
 void make_turn(const int size, char field[][size]);
 void checkAdding(const int add_cross,const int size,char field[][size]);
 int startTicTacToe ();
+void saveTicGame(int sizeF, char humanChar, int humanRow, int humanCol);
