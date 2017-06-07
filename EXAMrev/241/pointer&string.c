17f5158fb7d023f82aac7e6b4911723a2ff44412
hic <minping.yang@yahoo.com>
2017-06-07 22:19:29 +1200

    add three more method in array and pointer version

--- EXAMrev/241/pointer&string.c
+++ EXAMrev/241/pointer&string.c
@@ -5,9 +5,15 @@ int strcmp1(char*, char*);
 int main()
 {
     char s1[] = "11112222";
-    char s2[] = "11112223";
-    int i = strcmp2(s1,s2);
-    printf("%d\n",i);
+    char s2[] = "23";
+    // int i = strcmp2(s1,s2);
+    // printf("%d\n",i);
+    // strcpy2(s1,s2);
+    // puts(s2);
+    
+    printf("%d\n",strlen1(s2));
+   
+    
    
 }
 int strcmp1(char*s1, char*s2){
@@ -28,3 +34,23 @@ int strcmp2(char*s1, char*s2){
     }
     return *s1 - *s2;
 }
+
+void strcpy1(char*s1, char*s2){
+    while((*s2++ = *s1++)!='\0');
+}
+void strcpy2(char*s1, char*s2){
+    int i =0;
+    for(;s1[i]!='\0';i++){
+        s2[i] = s1[i];
+    }
+    
+}
+int strlen1(char *s){
+    int count = 0;
+    while(*s != '\0'){
+        s++;
+        count++;
+    }
+    return count;
+}
+
