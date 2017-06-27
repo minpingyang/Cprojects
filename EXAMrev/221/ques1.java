2e0bb6a99dd0498805f51a7c334cf5a77f1a14cb
hic <minping.yang@yahoo.com>
2017-06-27 23:14:19 +1200

    add inherient and overload example

--- EXAMrev/221/ques1.java
+++ EXAMrev/221/ques1.java
@@ -22,3 +22,56 @@ class ColouredPoint extends Point{int colour;}
 // – But, p refers to object with dynamic type ColouredPoint
 // – Can only access fields/methods exposed by static type of p
 // – Behaviour is determined by the dynamic type of p, though
+
+public class HelloWorld{
+    
+    public static void move(Point p){
+       System.out.println("sdfsd");
+    }
+    public static void paint(ColouredPoint p){
+      System.out.println("sdfsd");
+    }
+
+     public static void main(String []args){
+        Coloured3DPoint c3p = new Coloured3DPoint();
+        D3Point p3 = new D3Point();//work
+        Point point = new Point(); //work
+        paint(point); //doesnot work
+        
+     }
+}
+
+class Point{
+    int xpos;
+    int ypos;
+}
+class D3Point extends Point{int i;}
+class ColouredPoint extends Point{int j;}
+class Coloured3DPoint extends ColouredPoint{int x;}
+
+/*
+inheritance    
+*/
+
+
+/**overload metho**/
+public class HelloWorld{
+
+     public static void main(String []args){
+        A x = new A();
+        B y = new B();
+        x.ame(); //print aa
+        y.ame(); //print bb
+    
+     }
+}
+class A{
+    void ame(){
+        System.out.println("aa");
+    }
+}
+class B{
+     void ame(){
+       System.out.println("bb");
+    }
+}
