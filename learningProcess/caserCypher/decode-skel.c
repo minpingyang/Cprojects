6b857f4556ff28ed2fd5079c0bfa32eb9da21797
hic <minping.yang@yahoo.com>
2017-07-20 01:20:05 +1200

    version1.0 for decoding

--- learningProcess/caserCypher/decode-skel.c
+++ learningProcess/caserCypher/decode-skel.c
@@ -18,33 +18,137 @@ char upcase(char ch){
   return ch;
 }
 
-char* fixkey(char* s){
-  int i, j;
-  char plain[26]; // assume key < length of alphabet, local array on stack, will go away!
+// char* fixkey(char* s){
+//   int i, j;
+//   char plain[26]; // assume key < length of alphabet, local array on stack, will go away!
 
+//   for(i = 0, j = 0; i < strlen(s); i++){
+//     if(isalpha(s[i])){
+//       plain[j++] = upcase(s[i]);
+//     }
+//   }
+//   plain[j] = '\0'; 
+//   return strcpy(s, plain);
+// }
+
+
+// int in(char c, char* s, int pos){
+//   // Assume everything is already in the same case
+//   int i;
+
+//   for(i = 0; i < pos; i++)
+//     if(c == s[i]) return 1;
+    
+//   return 0;
+// } 
+
+
+char* fixkey(char* s){
+  int i, j;        
+  char plain[26]; //(!!probably need check the length of the key) assume key < length of alphabet, local array on stack, will go away!
+  
   for(i = 0, j = 0; i < strlen(s); i++){
-    if(isalpha(s[i])){
-      plain[j++] = upcase(s[i]);
+    char upLetter = upcase(s[i]);  //add condition to avoid duplicates
+    if(isalpha(s[i])&&isDuplicate(plain,upLetter)){ //skip punction
+      plain[j++] = upLetter;
     }
   }
   plain[j] = '\0'; 
   return strcpy(s, plain);
 }
 
-
-int in(char c, char* s, int pos){
-  // Assume everything is already in the same case
-  int i;
-
-  for(i = 0; i < pos; i++)
-    if(c == s[i]) return 1;
-    
+//check the next letter if it is already existing
+int isDuplicate(char* encode,char nextLett){ 
+  char* iniEncode = encode; //remember the initial key 
+ 
+  while(*encode != '\0'){//!!not sure if end with '\0'
+    if(*encode != nextLett){ // no duplicate case
+       encode++;
+    }else{//duplicate with key
+       return 1;
+    }
+  }
+  //now, *encode == '\0', then add the letter to encode
+  *encode = nextLett;
+  *(encode++) = '\0';
+  //point back to the first char of the key
+  encode = iniEncode;
   return 0;
+}
+
+char nextChar(char* encode,char finalCh){
+  char nextCh;
+  if(finalCh < 'Z'){ // to sure 
+    nextCh = final+1; 
+  }else{
+    nextCh = 'A'; 
+  }
+
+  int isDu = isDuplicate(encode,nextCh);
+  if(isDu ==0){//sans duplicate and update the encode
+    return nextCh;
+  }else{
+    nextChar(encode, nextCh);
+  }
 } 
 
+void buildtableEncode (char* key, char* encode){
 
-void buildtable (char* key, char* decode){ // this changed from encode
+  // This function needs to build an array of mappings in the 'encode' array from plaintext characters
+  // to encypered characters.  The encode array will be indexed by the plaintext char.  To 
+  // make this a useful 0-26 index for the array, 'A' will be stubtracted from it (yes you
+  // can do this in C).  You can see this in the main(){} below.  The values in the array 
+  // will be the cipher value, in the example at the top A -> H, B -> J, etc.
+
+  // You are implementing a Caesar 1 & 2 combo Cypher as given in handout.
+  // Your code here:
+
+  // probably need to declare some stuff here!
+  int offset= strlen(key);//the length couting white space and punctions
   
+  fixkey(key); // fix the key, i.e., uppercase and remove whitespace and punctuation
+  int index = offset+strlen(key)-2; // the index of final letter of fixedKey
+  // Do some stuff here to make a translation between plain and cypher maps.
+  //creat a array to store the key
+  //go through the key
+  encode = key;
+  //keep adding letter and updating encypered text
+  int len;
+  char nextCh;
+  char encypered[27];//include '\0' at the end
+  int i;
+  // store fixed key to the encrypted array in correct index.
+  for(i=offset-1;i<index;i++){
+    encypered[i] = *key;
+    key++;
+  }
+
+
+  while(len<26){
+    len = strlen(encode);
+    //find the final char of the key
+    char finalCh = *(encode+=(len-1));
+    nextCh = nextChar(encode,finalCh);
+    index++;
+    if(index==26){
+      index = 0;
+    }
+      encypered[index]=nextCh;
+  }
+  encypered[26]='\0';
+  strcpy(encode, encypered);
+}
+void buildtable (char* key, char* decode){ // this changed from encode
+  char* encode = decode;
+  buildtableEncode(key,encode);
+  int i = 0;
+  int orderIndex;
+  for(;i<26;i++){
+    char encyperedCh = encode[i];
+    orderIndex = encyperedCh - 'A';
+    decode[orderIndex]=encyperedCh;
+  }
+
   // This function needs to build an array of mappings in 'encode' from plaintext characters
   // to encihered characters.
 
@@ -53,7 +157,7 @@ void buildtable (char* key, char* decode){ // this changed from encode
 
   // probably need to declare some stuff here!
   
-  fixkey(key); // fix the key, i.e., uppercase and remove whitespace and punctuation
+  
 
   // the simplest way to do this is to do exactly the same as you did when creating the 
   // encode table, and then look up the encode table to get the translations, and build the
@@ -92,6 +196,7 @@ int main(int argc, char **argv){
     ch = fgetc(stdin);
     while (!feof(stdin)) {
       if(isalpha(ch))          // only decrypt alpha chars
+  ch = upcase(ch);
 	fputc(decode[ch-'A'], stdout);
      else 
 	fputc(ch, stdout);
